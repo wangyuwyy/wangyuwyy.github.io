5623912f01e032367de30d4d2a2c14fd9a56ad8b
wangyu666 <1750202198@qq.com>
2016-12-07 23:43:00 +0800

    20161202

--- works/dashu/js/js 2.js
+++ works/dashu/js/js 2.js
@@ -17,23 +17,28 @@
         var oClose = document.getElementById('close');
         var oB = document.getElementById('oBox');
         var ob = document.getElementById('opbox');
+        var tH = document.getElementById('tHeader');
         oNav.addEventListener('touchstart', function (ev) {
+            tH.style.display = 'block';
             oClose.style.display = 'block';
             oB.style.display = 'block';
             ob.style.display = 'block';
 			oB.style.opacity = '1';
         });
         oA.addEventListener('touchstart', function (ev) {
+            tH.style.display = 'none';
             oClose.style.display = 'none';
             ob.style.display = 'none';
 			oB.style.opacity = '0';
         });
         oT.addEventListener('touchstart', function (ev) {
+            tH.style.display = 'none';
             oClose.style.display = 'none';
             ob.style.display = 'none';
 			oB.style.opacity = '0';
         });
         oClose.addEventListener('touchstart', function (ev) {
+            tH.style.display = 'none';
             oClose.style.display = 'none';
             oB.style.display = 'none';
             ob.style.display = 'none';
